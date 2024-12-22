825b2d1527c22cde8811acc0aba165d21dbc5e79
Abhishek <103832942+cosmos-dx@users.noreply.github.com>
2024-12-22 21:00:22 +0530

    Hydration Error Correction

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { CartProvider } from "./components/CartContext";
 import Navbar from "./components/Navbar";
-
+const ClientOnly = dynamic(() => import('@/components/Wrappers/ClientSideOnly'), { ssr: false });
 export const metadata = {
   title: "E-commerce Website",
   description: "A sample e-commerce website built with Next.js",
@@ -11,10 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
+       <ClientOnly>
         <CartProvider>
           <Navbar />
           <main className="flex-grow">{children}</main>
         </CartProvider>
+       </ClientOnly>
       </body>
     </html>
   );
